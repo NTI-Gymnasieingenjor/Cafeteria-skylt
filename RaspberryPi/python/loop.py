a7f11498b2e1eed126a294a0429280dd07501b1d
pontusgranath <pontus.granath@learnet.se>
2020-11-03 13:39:32 +0100

    Fixed typo

--- RaspberryPi/python/loop.py
+++ RaspberryPi/python/loop.py
@@ -5,7 +5,7 @@ import subprocess
 #Automatically pulls from git every 10 seconds
 
 print("Pulling content")
-cmd = "cd /home/pi/Git/Cafeteria-skylt/ && git pull"
+cmd = "cd /home/pi/Git/Cafeteria-Skylt/ && git pull"
 returned_value = os.system(cmd)
 time.sleep(10)
 subprocess.Popen(['python3', '/home/pi/Git/Cafeteria-skylt/RaspberryPi/python/loop.py'])
