d58f04579e602c73e6ec96e5283c0bb1f289b826
Jonatan Möne <jonatan.mone@elev.ga.ntig.se>
2021-10-25 10:57:12 +0200

    Made tests more efficient and secure.

--- PyTests/tests.py
+++ PyTests/tests.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-from googleapiclient.discovery import build
-from google.oauth2 import service_account
-import json
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
@@ -10,39 +7,35 @@ from datetime import datetime
 import time
 from pathlib import Path
 import os
+import aiohttp
+import asyncio
 
 #Download the following extensions with these commands:
-#pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 #pip install selenium
+#pip install aiohttp
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SERVICE_ACCOUNT_FILE = '../keys.json'
-
-credentials = None
-credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-spreadsheet_id = "1x-orVp4FAC1rCucW2jtH5WTWgBSbgAaDLp23wa-V2fQ"
-service = build('sheets', 'v4', credentials=credentials)
 
 
 def Convert_to_usable_list(string):
     #Removes specified characters from the called string.
-    disallowed_characters = '{[","]}'
+    disallowed_characters = '["] \n'
     for character in string:
         for symbol in disallowed_characters:
             if character == symbol:
                 string = string.replace(character, "")
-    li = list(string.split(" "))
-    del li[0:5]
+    li = list(string.split(","))
 
     return li
 
-def Call_sheets_api():
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=spreadsheet_id, range="B4:E8").execute()
-    listed_result = json.dumps(result)
-    final_result = Convert_to_usable_list(listed_result)
 
-    return final_result
+async def main():
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://sheets.googleapis.com/v4/spreadsheets/1x-orVp4FAC1rCucW2jtH5WTWgBSbgAaDLp23wa-V2fQ/values/B4:E8?key=AIzaSyBPtjjvvCJ5Jy88dPjtlPXlsYCxGO8Kw7Q') as response:
+            html = await response.text()
+            raw_data = html[80:430]
+            processed_data = Convert_to_usable_list(raw_data)
+    return processed_data
 
 
 # removes non critical bug with browser and visualstudio
@@ -62,13 +55,15 @@ def checkForText(text):
 
 def TestWeekday():
     driver.get(codePath)
-    timeList = Call_sheets_api()
+    loop = asyncio.get_event_loop()
+    timeList = loop.run_until_complete(main())
+    
 
     for i in range(7):
+        driver.execute_script("day = " + str(i + 1) + "; GetOpenHours();")
+        time.sleep(1)
         if i <= 4:
             dayIndex = i * 4
-            driver.execute_script("day = " + str(i + 1) + "; GetOpenHours();")
-            time.sleep(1)
 
             day_start = str(timeList[dayIndex] + " - " + timeList[dayIndex+1])
             day_end = str(timeList[dayIndex+2] + " - " + timeList[dayIndex+3])
