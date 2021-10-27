1c07ecf7e27662d0e6f4f47b3ddc0c3a2645a6c6
Jonatan Möne <jonatan.mone@elev.ga.ntig.se>
2021-10-27 15:48:17 +0200

    Started making a function to change prices.

--- public/main.js
+++ public/main.js
@@ -40,5 +40,27 @@ function GetOpenHours(){
     setTimeout(GetOpenHours, 60000); //Update the openhours-status every minute 
 }
 
+
+function GetData(){
+    let range = "A16:A19";
+    let idList = ["products1", "products2", "products3, products4"]
+    for(let n = 0; n < idList.length; n++){
+        $.ajax({
+            type: 'GET',
+            url: "https://sheets.googleapis.com/v4/spreadsheets/1x-orVp4FAC1rCucW2jtH5WTWgBSbgAaDLp23wa-V2fQ/values/'Datahantering'!" + range + "?key=AIzaSyBPtjjvvCJ5Jy88dPjtlPXlsYCxGO8Kw7Q#gid=350188528",
+
+            success: function (data) {
+                let products = data.values;
+                let productFull = products.filter(function (item){ return item != ''});
+                for(let i = 0; i < productFull.length; i++){
+                    document.getElementById(idList[n]).children[i].innerHTML= productFull[i];
+                }
+            }
+        });
+    }
+}
+
+
 StartTime();
 GetOpenHours();
+GetData();
