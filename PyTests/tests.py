b506912aa3e479c02f5fe6df0c2e51fbe9cd6ec4
Simon Clavensjö <simon.clavensjo@elev.ntig.se>
2023-10-09 11:44:03 +0200

    update selenium and use unitest

--- PyTests/tests.py
+++ PyTests/tests.py
@@ -6,50 +6,75 @@ from selenium.webdriver.chrome.options import Options
 from datetime import datetime
 import time
 from pathlib import Path
-import os
 import requests
 import asyncio
+from os import getcwd, path
+from unittest import TestCase, main
+from selenium.webdriver.common.by import By
+
 
 # Download the following extensions with these commands:
 # pip install selenium
 
 
+class TestHomepageENG(TestCase):
+    doNotCloseBrowser = False
+    hideWindow = True
 
-# Removes non critical bug with browser and Visual Studio
-options = webdriver.ChromeOptions()
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome( options = options)
+    @classmethod
+    def setUpClass(cls):
+        chr_options = Options()
+        
+        chr_options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
-# Gets current parent directory
-cwd = Path(os.getcwd()).parent
+        if cls.doNotCloseBrowser:
+            chr_options.add_experimental_option("detach", True)
 
-# Gets code file path way
-codePath = str(cwd) + '/public/index.html'
+        if cls.hideWindow:
+            chr_options.add_argument("--headless")
 
-# Checking for text on site
-def checkForText(text):
-    assert text in driver.find_element_by_xpath("/html/body").text
+        cls.browser = webdriver.Chrome(options=chr_options)
 
-# Tests that time is correct on Monday-Friday and that it's closed on weekends
-def TestWeekdays():
-    driver.get(codePath)
-    print("Loading site...")
-    response = requests.get("https://sheets.googleapis.com/v4/spreadsheets/1x-orVp4FAC1rCucW2jtH5WTWgBSbgAaDLp23wa-V2fQ/values/'%C3%96ppettider'!B4:C8?key=AIzaSyBPtjjvvCJ5Jy88dPjtlPXlsYCxGO8Kw7Q")
-    timeList = response.json()
+    @classmethod
+    def tearDownClass(cls):
+        pass
 
-    for i in range(0, 7):
-        # Sets the day in the JavaScript code
-        driver.execute_script("day = " + str(i) + "; GetOpenHours();")
-        time.sleep(1)
-        # Number 0 is sunday and 6 is saturday
-        if i <= 5 and i > 0:
-            dayStart = str(timeList["values"][i-1][0] + " - " + timeList["values"][i-1][1])
-            checkForText(dayStart)
-        else:
-            checkForText("Måndag - Fredag")
-        print("Day " + str(i) + ": success")
-        driver.refresh()
-    driver.close()
+    def setUp(self):
+        self.browser.get(path.join(getcwd(), "./public/index.html"))
 
-TestWeekdays()
-print("Test successful!")
+    def tearDown(self):
+        self.browser.get("about:blank")
+
+    # Checking for text on site
+    def testTitle(self):
+        self.assertEqual("Prislista", self.browser.title)
+
+    def checkForText(self,text):
+        assert text in self.browser.find_element(By.XPATH,"/html/body").text
+
+    # Tests that time is correct on Monday-Friday and that it's closed on weekends
+    def testWeekdays(self):
+        print("Loading site...")
+        response = requests.get(
+            "https://sheets.googleapis.com/v4/spreadsheets/1x-orVp4FAC1rCucW2jtH5WTWgBSbgAaDLp23wa-V2fQ/values/'%C3%96ppettider'!B4:C8?key=AIzaSyBPtjjvvCJ5Jy88dPjtlPXlsYCxGO8Kw7Q"
+        )
+        timeList = response.json()
+
+        for i in range(0, 7):
+            # Sets the day in the JavaScript code
+            self.browser.execute_script("day = " + str(i) + "; GetOpenHours();")
+            time.sleep(1)
+            # Number 0 is sunday and 6 is saturday
+            if i <= 5 and i > 0:
+                dayStart = str(
+                    timeList["values"][i - 1][0] + " - " + timeList["values"][i - 1][1]
+                )
+                self.checkForText(dayStart)
+            else:
+                self.checkForText("Måndag - Fredag")
+            print("Day " + str(i) + ": success")
+            self.browser.refresh()
+        self.browser.close()
+
+if __name__ == "__main__":
+    main(verbosity=2)
