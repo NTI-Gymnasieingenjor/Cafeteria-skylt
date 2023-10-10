0b0dd57bf32bae1a426d424999d088ad8b3f76c5
lukasgurra <142992155+lukasgurra@users.noreply.github.com>
2023-10-10 14:38:06 +0200

    Remove unneccessary prints
    Co-Authored-By: Fabian-Hemstrom <142995105+Fabian-Hemstrom@users.noreply.github.com>
    Co-Authored-By: Simon Clavensjö <142975808+SimonClavensjo@users.noreply.github.com>

--- RaspberryPi/temperatureLogger.py
+++ RaspberryPi/temperatureLogger.py
@@ -8,9 +8,6 @@ proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 o, e = proc.communicate()
 
-print("output: " + o.decode("ascii"))
-print("Error: " + e.decode("ascii"))
-print("code: " + str(proc.returncode))
 
 with open("temperature_data.csv", "a") as file:
     file.write(current_time + " " + o.decode("ascii"))
