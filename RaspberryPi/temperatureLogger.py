7457845c8fd615ee4a4450d1b8bc29e82a3785b2
Simon Clavensjö <simon.clavensjo@elev.ntig.se>
2023-10-11 11:38:06 +0200

    add restart if temp >= 80

--- RaspberryPi/temperatureLogger.py
+++ RaspberryPi/temperatureLogger.py
@@ -1,14 +1,22 @@
+import os
+import re
 import subprocess
 from datetime import datetime
 
 now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
+currentTime = now.strftime("%H:%M:%S")
 cmd = ["vcgencmd", "measure_temp"]
 proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 o, e = proc.communicate()
 
+result = o.decode("ascii")
+pattern = "\d{2}"
+match = int(re.search(pattern, result).group())
 
 with open("temperature_data.csv", "a") as file:
-    file.write(current_time + " " + o.decode("ascii"))
+    file.write(f"{currentTime}  {result}")
     file.close()
+
+if match >= 80 :
+    os.system("sudo reboot")
