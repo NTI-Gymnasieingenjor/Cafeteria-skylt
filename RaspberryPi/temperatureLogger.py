70644760b5a997c260958b05869983144253ee97
Simon Clavensjö <simon.clavensjo@elev.ntig.se>
2023-10-11 11:54:39 +0200

    add more comments

--- RaspberryPi/temperatureLogger.py
+++ RaspberryPi/temperatureLogger.py
@@ -14,12 +14,16 @@ proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 o, e = proc.communicate()
 
 result = o.decode("ascii")
+# Find the temperature in the string by looking for two digits in a row
 pattern = "\d{2}"
 match = int(re.search(pattern, result).group())
 
+# Log the temperature and time to "temperature_data.csv"
 with open("temperature_data.csv", "a") as file:
     file.write(f"{currentTime}  {result}")
     file.close()
 
+
+# If the temperature is above 80 degrees celsius, reboot the system
 if match >= 80 :
     os.system("sudo reboot")
