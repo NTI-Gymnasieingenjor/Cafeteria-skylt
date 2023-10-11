9c3ad4b724d3683ab4e891620ed87936ec8de034
Fabian-Hemstrom <142995105+Fabian-Hemstrom@users.noreply.github.com>
2023-10-11 14:56:52 +0200

    add weekday and date test
    Co-Authored-By: lukasgurra <142992155+lukasgurra@users.noreply.github.com>
    Co-Authored-By: Simon Clavensjö <142975808+SimonClavensjo@users.noreply.github.com>

--- PyTests/testDateTime.py
+++ PyTests/testDateTime.py
@@ -1,4 +1,3 @@
-import time
 from os import getcwd, path
 from unittest import TestCase, main
 
@@ -82,3 +81,99 @@ class TestTime(TestCase):
         self.checkTime("2023-05-14T11:00:00", "11:00")
         self.checkTime("2023-05-14T13:00:00", "13:00")
         self.checkTime("2023-05-14T15:00:00", "15:00")
+
+    def checkDate(self, date, result):
+        self.browser.execute_script(f"todaysDate(new Date('{date}'));")
+        shownDate = self.browser.find_element(By.ID, "date").text
+        self.assertEqual(shownDate, result)
+
+    def testDate(self):
+        # Monday
+        self.checkDate("2023-05-08T00:00:00", "2023-05-08")
+        self.checkDate("2023-05-08T08:00:00", "2023-05-08")
+        self.checkDate("2023-05-08T15:45:00", "2023-05-08")
+        self.checkDate("2023-05-08T23:59:00", "2023-05-08")
+
+        # Tuesday
+        self.checkDate("2023-05-09T00:00:00", "2023-05-09")
+        self.checkDate("2023-05-09T08:00:00", "2023-05-09")
+        self.checkDate("2023-05-09T15:45:00", "2023-05-09")
+        self.checkDate("2023-05-09T23:59:00", "2023-05-09")
+
+        # Wednesday
+        self.checkDate("2023-05-10T00:00:00", "2023-05-10")
+        self.checkDate("2023-05-10T08:00:00", "2023-05-10")
+        self.checkDate("2023-05-10T15:45:00", "2023-05-10")
+        self.checkDate("2023-05-10T23:59:00", "2023-05-10")
+
+        # Thursday
+        self.checkDate("2023-05-11T00:00:00", "2023-05-11")
+        self.checkDate("2023-05-11T08:00:00", "2023-05-11")
+        self.checkDate("2023-05-11T15:45:00", "2023-05-11")
+        self.checkDate("2023-05-11T23:59:00", "2023-05-11")
+
+        # Friday
+        self.checkDate("2023-05-12T00:00:00", "2023-05-12")
+        self.checkDate("2023-05-12T08:00:00", "2023-05-12")
+        self.checkDate("2023-05-12T15:45:00", "2023-05-12")
+        self.checkDate("2023-05-12T23:59:00", "2023-05-12")
+
+        # Saturday
+        self.checkDate("2023-05-13T00:00:00", "2023-05-13")
+        self.checkDate("2023-05-13T08:00:00", "2023-05-13")
+        self.checkDate("2023-05-13T15:45:00", "2023-05-13")
+        self.checkDate("2023-05-13T23:59:00", "2023-05-13")
+
+        # Sunday
+        self.checkDate("2023-05-14T00:00:00", "2023-05-14")
+        self.checkDate("2023-05-14T08:00:00", "2023-05-14")
+        self.checkDate("2023-05-14T15:45:00", "2023-05-14")
+        self.checkDate("2023-05-14T23:59:00", "2023-05-14")
+
+    def checkWeekday(self, date, result):
+        self.browser.execute_script(f"todaysDate(new Date('{date}'));")
+        shownWeekday = self.browser.find_element(By.ID, "day").text
+        self.assertEqual(shownWeekday, result)
+
+    def testWeekday(self):
+        # Monday
+        self.checkWeekday("2023-05-08T00:00:00", "Måndag")
+        self.checkWeekday("2023-05-08T08:00:00", "Måndag")
+        self.checkWeekday("2023-05-08T15:45:00", "Måndag")
+        self.checkWeekday("2023-05-08T23:59:00", "Måndag")
+
+        # Tuesday
+        self.checkWeekday("2023-05-09T00:00:00", "Tisdag")
+        self.checkWeekday("2023-05-09T08:00:00", "Tisdag")
+        self.checkWeekday("2023-05-09T15:45:00", "Tisdag")
+        self.checkWeekday("2023-05-09T23:59:00", "Tisdag")
+
+        # Wednesday
+        self.checkWeekday("2023-05-10T00:00:00", "Onsdag")
+        self.checkWeekday("2023-05-10T08:00:00", "Onsdag")
+        self.checkWeekday("2023-05-10T15:45:00", "Onsdag")
+        self.checkWeekday("2023-05-10T23:59:00", "Onsdag")
+
+        # Thursday
+        self.checkWeekday("2023-05-11T00:00:00", "Torsdag")
+        self.checkWeekday("2023-05-11T08:00:00", "Torsdag")
+        self.checkWeekday("2023-05-11T15:45:00", "Torsdag")
+        self.checkWeekday("2023-05-11T23:59:00", "Torsdag")
+
+        # Friday
+        self.checkWeekday("2023-05-12T00:00:00", "Fredag")
+        self.checkWeekday("2023-05-12T08:00:00", "Fredag")
+        self.checkWeekday("2023-05-12T15:45:00", "Fredag")
+        self.checkWeekday("2023-05-12T23:59:00", "Fredag")
+
+        # Saturday
+        self.checkWeekday("2023-05-13T00:00:00", "Lördag")
+        self.checkWeekday("2023-05-13T08:00:00", "Lördag")
+        self.checkWeekday("2023-05-13T15:45:00", "Lördag")
+        self.checkWeekday("2023-05-13T23:59:00", "Lördag")
+
+        # Sunday
+        self.checkWeekday("2023-05-14T00:00:00", "Söndag")
+        self.checkWeekday("2023-05-14T08:00:00", "Söndag")
+        self.checkWeekday("2023-05-14T15:45:00", "Söndag")
+        self.checkWeekday("2023-05-14T23:59:00", "Söndag")
