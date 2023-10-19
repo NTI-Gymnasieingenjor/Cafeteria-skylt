7746635556cdf50df278a2f4da1b8a49d588a439
Simon Clavensjö <simon.clavensjo@elev.ntig.se>
2023-10-19 11:54:07 +0200

    make startServer independent to os

--- RaspberryPi/configuration/startServer.py
+++ RaspberryPi/configuration/startServer.py
@@ -1,7 +1,13 @@
 import os
+import platform
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from os import getcwd, path
 
-os.chdir('/home/pi/Git/Cafeteria-skylt/public')
-serverAddress = ("", 8000)  
+if platform.system() == "Windows":
+    cwd = getcwd()
+    os.chdir(cwd + "/public")
+elif platform.system() == "Linux":
+    os.chdir("../../public")
+serverAddress = ("", 8080)
 httpd = HTTPServer(serverAddress, SimpleHTTPRequestHandler)
 httpd.serve_forever()
