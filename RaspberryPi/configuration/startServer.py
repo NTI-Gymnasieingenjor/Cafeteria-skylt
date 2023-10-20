dfcae5ccf9ee5abae5974aef84b8e6f4b6f9000f
Simon Clavensjö <simon.clavensjo@elev.ntig.se>
2023-10-20 14:24:53 +0200

    change to small c

--- RaspberryPi/configuration/startServer.py
+++ RaspberryPi/configuration/startServer.py
@@ -7,7 +7,7 @@ if platform.system() == "Windows":
     cwd = getcwd()
     os.chdir(cwd + "/public")
 elif platform.system() == "Linux":
-    os.chdir("/home/pi/Git/Cafeteria-display/public")
+    os.chdir("/home/pi/Git/cafeteria-display/public")
 serverAddress = ("", 8000)
 httpd = HTTPServer(serverAddress, SimpleHTTPRequestHandler)
 httpd.serve_forever()
